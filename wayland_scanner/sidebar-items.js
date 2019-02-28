ff0567f2d552a03daee21d63938d2e39b81f8555
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-28 16:32:01 +0000

    Deploy Smithay/wayland-rs to github.com/Smithay/wayland-rs.git:gh-pages

---
+++ wayland_scanner/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["Side","Side to generate"]],"fn":[["generate_code","Generate the code for a protocol using the C system libs"],["generate_code_streams","Generate the code for a protocol from/to IO streams using the C system libs"]]});
